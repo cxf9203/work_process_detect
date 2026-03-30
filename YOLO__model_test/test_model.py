bd782f84e737de4b784f8b0373544f112c54aef2
unknown <email@gmail.com>
2026-03-30 22:36:56 +0800

    add python yolo test model and scripts

---
+++ YOLO__model_test/test_model.py
@@ -0,0 +1,36 @@
+from ultralytics   import YOLO
+import cv2
+model = YOLO("YOLO_test/best.pt")
+#查看model的label信息
+#print(model.names)
+#{0: 'GEAR', 1: 'LD_luosi', 2: 'PUT_chilun', 3: 'RD_luosi', 4: 'SCREW', 5: 'YS_luosi', 6: 'ZS_luosi', 7: 'keti'}
+labelSet = ["GEAR","LD_luosi","PUT_chilun","RD_luosi","SCREW","YS_luosi","ZS_luosi","keti"]
+video_path = "video/work_process.mp4"
+image_path = "saved_img/frame_0007.jpg"
+#model.predict(source=video_path, save=True, show=True)
+results = model.predict(source=image_path, save=True, show=True)
+img = cv2.imread(image_path)
+print(results)#打印结果
+#使用官方yolo方式将标签信息显示在图片上
+cv2.imshow("result", results[0].plot())
+cv2.waitKey(0)
+
+
+#有哪些检测到的label请打印出来
+# Draw the bounding boxes and labels on the frame
+for result in results:
+    boxes = result.boxes
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        labelidx = box.cls[0].item()
+        label = labelSet[int(labelidx)]
+        confidence = box.conf[0].item()
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(img, f"{label} {confidence:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+# Display the frame
+cv2.imshow("YOLOv8 Object Detection", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
